196dfcd83bc8baa7afde29acc498cf3761261cb6
Rishi <Rishi@LAPTOP-ANUIA775>
2025-11-05 00:09:34 +0530

    first commit

---
+++ javaaprojects/src/mypack/Studentmain.java
@@ -0,0 +1,20 @@
+package mypack;
+
+public class Studentmain {
+
+	public static void main(String[] args) {
+		 Student s1 = new Student();
+	        s1.name="ashrita";
+	        s1.age=18;
+	        s1.marks=97;
+	        s1.display();
+	        s1.name="asmita";
+	        Student s2 = new Student();
+	        s2.name="ashmitha";
+	        s2.age=18;
+	        s2.marks=98;
+	        s2.display();
+
+	}
+
+}
