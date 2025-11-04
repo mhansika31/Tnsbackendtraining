21526399fde65affa6a54dfba5f3a5adb94479af
Rishi <Rishi@LAPTOP-ANUIA775>
2025-11-04 23:37:00 +0530

    first commit

---
+++ javaaprojects/src/mypack/Employeemain.java
@@ -0,0 +1,18 @@
+package mypack;
+
+public class Employeemain {
+
+	public static void main(String[] args) {
+		Employee e1 = new Employee();
+        e1.sname="hansika";
+        e1.id=101;
+        e1.salary=70000;
+        e1.display();
+        Employee e2 = new Employee();
+        e2.sname="harish";
+        e2.id=102;
+        e2.salary=65000;
+        e2.display();
+	}
+
+}
