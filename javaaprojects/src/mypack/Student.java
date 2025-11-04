9a6da3ff5d053b0081c22f669787f99d22c5b6c7
Rishi <Rishi@LAPTOP-ANUIA775>
2025-11-04 23:50:22 +0530

    first commit

---
+++ javaaprojects/src/mypack/Student.java
@@ -0,0 +1,12 @@
+package mypack;
+
+public class Student {
+	String name;
+    int age;
+    int marks;
+    void display() {
+    	System.out.println("Student Name: " + name + " Age: " + age + " Marks: " + marks);
+
+    }
+
+}
