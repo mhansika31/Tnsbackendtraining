765d84b5a2d56d6233286beba4b2aa684a226e0f
Rishi <Rishi@LAPTOP-ANUIA775>
2025-11-05 00:46:49 +0530

    commit

---
+++ javaaprojects/src/mypack/Forloop.java
@@ -0,0 +1,11 @@
+
+		package mypack;
+
+		public class Forloop {
+		    public static void main(String[] args) {
+		        for (int i = 1; i <= 5; i++) {
+		            System.out.println(i);
+		        }
+		    }
+
+}
