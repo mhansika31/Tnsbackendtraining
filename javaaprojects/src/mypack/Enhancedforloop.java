60f4420de2d077240603eea2342fded21aab63d7
Rishi <Rishi@LAPTOP-ANUIA775>
2025-11-05 01:13:25 +0530

    commit

---
+++ javaaprojects/src/mypack/Enhancedforloop.java
@@ -0,0 +1,11 @@
+package mypack;
+
+public class Enhancedforloop {
+    public static void main(String[] args) {
+        int[] numbers = {10, 20, 30, 40, 50};
+
+        for (int num : numbers) {
+            System.out.println(num);
+        }
+    }
+}
