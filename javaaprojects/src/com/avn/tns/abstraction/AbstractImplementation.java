ef72762b55222a43e5b60712ac15fe8d672dafae
hansika <mattahansika@gmail.com>
2025-11-11 22:23:40 +0530

    Update AbstractImplementation.java

--- javaaprojects/src/com/avn/tns/abstraction/AbstractImplementation.java
+++ javaaprojects/src/com/avn/tns/abstraction/AbstractImplementation.java
@@ -6,6 +6,10 @@ public class AbstractImplementation extends AbstractDemo {
     // Implementing the abstract method
     @Override
      void calculateArea() {
+         demo.display();
+		 demo.calculateArea();
+		 System.out.println("Abstraction demo completed successfully!");
         
     }
 }
+
