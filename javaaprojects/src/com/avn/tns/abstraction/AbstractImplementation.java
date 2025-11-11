b005bdb134c817bb4951c1b747984d96ea65ae67
hansika <mattahansika@gmail.com>
2025-11-11 22:24:31 +0530

    Update AbstractImplementation.java

--- javaaprojects/src/com/avn/tns/abstraction/AbstractImplementation.java
+++ javaaprojects/src/com/avn/tns/abstraction/AbstractImplementation.java
@@ -6,10 +6,12 @@ public class AbstractImplementation extends AbstractDemo {
     // Implementing the abstract method
     @Override
      void calculateArea() {
-         demo.display();
-		 demo.calculateArea();
-		 System.out.println("Abstraction demo completed successfully!");
+         System.out.println("Calculating area inside AbstractImplementation class...");
+        double radius = 5.0;
+        double area = Math.PI * radius * radius;
+        System.out.println("Area of circle with radius " + radius + " is " + area);
         
     }
 }
+
 
