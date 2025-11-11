890a78ade7482ece6dbe6b37a4758fcee5b9da12
hansika <mattahansika@gmail.com>
2025-11-11 22:25:10 +0530

    Update AbstractMain.java

--- javaaprojects/src/com/avn/tns/abstraction/AbstractMain.java
+++ javaaprojects/src/com/avn/tns/abstraction/AbstractMain.java
@@ -4,7 +4,11 @@ public class AbstractMain {
 
 	public static void main(String[] args) {
 		AbstractDemo ob = new AbstractImplementation();
+		demo.display();
+		demo.calculateArea();
+		System.out.println("Abstraction demo completed successfully!");
 
 	}
 
 }
+
