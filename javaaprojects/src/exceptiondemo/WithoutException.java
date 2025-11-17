07b14ff84f5470c537f7591f30466565af0d5053
hansika <mattahansika@gmail.com>
2025-11-17 20:28:25 +0530

    without exception handling

---
+++ javaaprojects/src/exceptiondemo/WithoutException.java
@@ -0,0 +1,10 @@
+package exceptiondemo;
+
+public class WithoutException {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 0;
+        int divide = a / b;
+        System.out.println("The result is " + divide);
+    }
+}
